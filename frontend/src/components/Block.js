d4aac4b2d21d631122f5c64a9349573c4a6d37fb
nishanthmhegde <=>
2020-08-27 19:53:14 +0530

    Added toggle able transaction data display

--- frontend/src/components/Block.js
+++ frontend/src/components/Block.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Button} from 'react-bootstrap';
 import {MILLISECONDS} from '../config';
 import Transactions from './Transactions';
+
+//TransactionToggle component will be receiving a prop object from Block component which would be the data JSON inside the block
+function TransactionToggle({data}){
+	var [toggleDisplay, setToggleDisplay] = useState(false);
+
+	const toggleDisplayHandler = (event) => {
+		setToggleDisplay(!toggleDisplay);
+	};
+	//display the transaction information only when the button Show More is clicked.
+	if (toggleDisplay){
+		return (
+			<div>
+		{
+          data.map(transaction => (
+            <div key={transaction.id}>
+              <hr />
+              <Transactions transactions={transaction} />
+            </div>
+          ))
+        }
+		<Button variant="danger" size="sm" onClick={toggleDisplayHandler}>Show Less</Button>
+        </div>
+        );
+	}
+	else {
+		return (
+		<Button variant="danger" size="sm" onClick={toggleDisplayHandler}>Show More</Button>
+		);
+	}
+
+
+}
 
 //Block component will be receiving a prop object from Blockchain component
 function Block({block}){
@@ -19,14 +52,7 @@ function Block({block}){
 		<div>Date:{timestampDisplay}</div>
 		<div>Hash:{hashDisplay}</div>
 		<div>Previous Hash:{lastHashDisplay}</div>
-		{
-          data.map(transaction => (
-            <div key={transaction.id}>
-              <hr />
-              <Transactions transactions={transaction} />
-            </div>
-          ))
-        }
+		<div><TransactionToggle data={data} /></div>
 		</div>
 		);
 }
